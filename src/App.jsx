df3407a0306fa8058c960c1e629e56c5327eb5d7
marine <grigorianm05.ait@gmail.com>
2022-07-21 15:03:25 +0400

    Created tag div content and first part of content with large text. Function CreateButton is created and header's button is replaced by it. Created and used class colorDarkBlue, colorDarkGrey. Added jpg-file and used as background-image in div content

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 // import "./font-awesome.css";
 import React from 'react';
@@ -9,21 +8,36 @@ class OnePage extends React.Component{
     return (
       <div>
         <header className='header'>
-          <div className='logo'>OnePage</div>
+          <div className='logo colorDarkBlue'>OnePage</div>
           <div className='nav'>
             <ul className='menu'>
               {menuElements.map((menuEl, i) => 
-                i !== 6 ? <li key={`MenuEl ${i}`} className={i === 0 ? "colorOfMenuEl" : "nothing"}>
-                {menuEl}</li> : <li key={`MenuEl ${i}`}>{menuEl} <span className='drop'>&#x25BC;</span></li>
+                i !== 6 ? <li key={`MenuEl ${i}`} className={i === 0 ? "colorOfMenuEl" : "colorDarkGrey"}>
+                {menuEl}</li> : <li key={`MenuEl ${i}`} className="colorDarkGrey">{menuEl} <span className='drop'>&#x25BC;</span></li>
                 // <i class="fa fa-chevron-down" aria-hidden="true"></i>
               )}
             </ul>
-            <button className='button'>Get started</button>
+            {createButton()}
+            {/* <button className='button'>Get started</button> */}
           </div>
         </header>
+        <div className='content'>
+          <div className='firstPartOfContentWithLargeText'>
+            <p className='largeText colorDarkBlue'>One Page Bootstrap</p>
+            <p className='largeText colorDarkBlue'>Website Template</p>
+            <p className='textTeamDesigners colorDarkGrey'>We are team of talanted designers</p>
+            {createButton()}
+          </div>
+        </div>
       </div>
     )
   }
+}
+
+function createButton() {
+  return  ( // при создании react компонента функция не читается
+    <button className='button'>Get started</button>
+  )
 }
 
 
